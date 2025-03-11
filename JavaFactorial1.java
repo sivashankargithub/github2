3a3660d1f74c98b1fa8db44fe5160dd26a6f8e23
Sivashankar C <sivashankarchandrappa.sangamone@gmail.com>
2025-03-11 19:33:00 +0530

    Error in Java ClassNotFoundException

--- JavaFactorial1.java
+++ JavaFactorial1.java
@@ -1,6 +1,12 @@
-package pkg1;
-
 public class JavaFactorial1 {
+	public static int findFactorial(int num1){
+		int fact1 =1;
+		for(int i=1;i<num1+1;i++) {
+			fact1=fact1*i;
+		}
+		return fact1;
+	}
+
 	public static void main(String[] args) {
 		int num1 =5;
 		int fact1 =1;
@@ -8,6 +14,8 @@ public class JavaFactorial1 {
 			fact1=fact1*i;
 		}
 		System.out.println("Factorial: "+fact1);
+		int factorial1=findFactorial(3);
+		System.out.println(factorial1);
 	}
 
 }
